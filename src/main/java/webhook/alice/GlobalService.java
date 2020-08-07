d7a75ffe8df6192b21e4692ea42c6d58e28904e3
Ethan Sung <baegjae@gmail.com>
2020-08-07 17:18:08 +0900

    Refactor webhook faber/alice
    Signed-off-by: Ethan Sung <baegjae@gmail.com>

--- src/main/java/webhook/alice/GlobalService.java
+++ src/main/java/webhook/alice/GlobalService.java
@@ -1,6 +1,8 @@
 package webhook.alice;
 
 import com.evernym.sdk.vcx.connection.ConnectionApi;
+import com.evernym.sdk.vcx.credential.CredentialApi;
+import com.evernym.sdk.vcx.proof.DisclosedProofApi;
 import com.evernym.sdk.vcx.utils.UtilsApi;
 import com.evernym.sdk.vcx.vcx.VcxApi;
 import com.evernym.sdk.vcx.wallet.WalletApi;
@@ -9,17 +11,24 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 import utils.Common;
+import utils.NotificationsRequestDto;
+import utils.VcxState;
 
 import javax.annotation.PostConstruct;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedHashMap;
 import java.util.logging.Logger;
 
-import static utils.Common.prettyJson;
-import static utils.Common.requestGET;
+import static utils.Common.*;
+import static utils.Common.getHash;
 
 @Service
 public class GlobalService {
     // get logger for demo - INFO configured
     static final Logger logger = Common.getDemoLogger();
+    static final String tailsFileRoot = System.getProperty("user.home") + "/.indy_client/tails";
+    static final String tailsServerUrl = "http://13.124.169.12";
 
     static final String webhookUrl = "http://localhost:7202/notifications"; // alice (me)
     static final String invitationUrl = "http://localhost:7201/invitations"; // faber
@@ -81,5 +90,216 @@ public class GlobalService {
         logger.config("addRecordWallet (connection, " + pwDid + ", " + prettyJson(connection) + ")");
         WalletApi.addRecordWallet("connection", pwDid, connection, "").get();
         ConnectionApi.connectionRelease(connectionHandle);
+    }
+
+    public void handleMessage(NotificationsRequestDto body) throws Exception {
+        logger.info("handleMessage >>> body: " + body.toString());
+
+        // Get the message from mediator using notification information
+        String messages = UtilsApi.vcxGetMessages(body.getMsgStatusCode(), body.getMsgUid(), body.getPwDid()).get();
+        String msgUid = body.getMsgUid();
+        logger.info( "Messages: " +  messages);
+        String message = JsonPath.parse((LinkedHashMap)JsonPath.read(messages,"$.[0].msgs[0]")).jsonString();
+        String decryptedPayload = JsonPath.read(message, "$.decryptedPayload");
+        String payloadMessage = JsonPath.read(decryptedPayload,"$.@msg");
+        String type = JsonPath.read(decryptedPayload,"$.@type.name");
+
+        // pwDid is used as a connectionId
+        String pwDid = JsonPath.read(messages,"$.[0].pairwiseDID");
+        String connectionRecord = WalletApi.getRecordWallet("connection", pwDid, "").get();
+        String serializedConnection = JsonPath.read(connectionRecord,"$.value");
+        int connectionHandle = ConnectionApi.connectionDeserialize(serializedConnection).get();
+
+        switch(type) {
+            case "aries":
+                String innerType = JsonPath.read(payloadMessage,"$.@type");
+
+                // STEP.4 - connection created
+                if(innerType.equals("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/response")){
+                    logger.info("- Case(aries ,connections/1.0/response) -> sendConnectionAck");
+                    sendConnectionAck(connectionHandle, pwDid);
+                }
+                // STEP.13 - receive proof ACK
+                else if (innerType.equals("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/present-proof/1.0/ack")){
+                    logger.info("- Case(aries ,present-proof/1.0/ack) -> receiveProofAck");
+                    receiveProofAck(connectionHandle, payloadMessage);
+                }
+                else {
+                    logger.severe("Unknown innerType message in aries type");
+                }
+                break;
+            // STEP.7 - accept credential offer & request credential
+            case "credential-offer":
+                logger.info("- Case(credential-offer) -> sendCredentialRequest");
+                sendCredentialRequest(connectionHandle, pwDid, msgUid);
+                break;
+            // STEP.9 - accept credential
+            case "credential":
+                logger.info("- Case(credential) -> acceptCredential");
+                acceptCredential(connectionHandle, payloadMessage);
+                break;
+            // STEP.11 - send proof
+            case "presentation-request":
+                logger.info("- Case(presentation-request) -> sendProof");
+                sendProof(connectionHandle, pwDid, msgUid);
+                break;
+            default:
+                logger.severe("Unknown type message");
+
+        }
+        ConnectionApi.connectionRelease(connectionHandle);
+    }
+
+    void sendConnectionAck(int connectionHandle, String pwDid) throws Exception {
+        int connectionState = ConnectionApi.vcxConnectionUpdateState(connectionHandle).get();
+
+        if (connectionState == VcxState.Accepted.getValue()) {
+            String serializedConnection = ConnectionApi.connectionSerialize(connectionHandle).get();
+            logger.config("updateRecordWallet (connection, " + pwDid + ", " + prettyJson(serializedConnection) + ")");
+            WalletApi.updateRecordWallet("connection", pwDid, serializedConnection).get();
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+    }
+
+    void sendCredentialRequest(int connectionHandle, String pwDid, String msgUid) throws Exception {
+        String offers = CredentialApi.credentialGetOffers(connectionHandle).get();
+        String credentialOffer = JsonPath.parse((LinkedHashMap)JsonPath.read(offers, "$.[0]")).jsonString();
+        logger.info("credential offer:\n" + prettyJson(credentialOffer));
+
+        // Update agency message status manually (xxxUpdateState automatically update message status, but not here)
+        UtilsApi.vcxUpdateMessages("MS-106",
+                "[{\"pairwiseDID\":\"" + pwDid + "\",\"uids\":[\"" + msgUid + "\"]}]");
+
+        // Create a credential object from the credential offer
+        int credentialHandle = CredentialApi.credentialCreateWithOffer("credential", credentialOffer).get();
+
+        logger.info("#15 After receiving credential offer, send credential request");
+        CredentialApi.credentialSendRequest(credentialHandle, connectionHandle, 0).get();
+
+        //Serialize the object
+        String serializedCredential = CredentialApi.credentialSerialize(credentialHandle).get();
+        String threadId = JsonPath.read(serializedCredential, "$.data.holder_sm.thread_id");
+        logger.config("addRecordWallet (credential, " + threadId + ", " + prettyJson(serializedCredential) + ")");
+        WalletApi.addRecordWallet("credential", threadId, serializedCredential, "").get();
+
+        CredentialApi.credentialRelease(credentialHandle);
+    }
+
+    void acceptCredential(int connectionHandle, String payloadMessage) throws Exception {
+        String threadId = JsonPath.read(payloadMessage, "$.~thread.thid");
+        String credentialRecord = WalletApi.getRecordWallet("credential", threadId, "").get();
+        String serializedCredential = JsonPath.read(credentialRecord, "$.value");
+
+        // TODO: Must replace connection_handle in credential - Need to consider better way
+        serializedCredential = JsonPath.parse(serializedCredential)
+                .set("$.data.holder_sm.state.RequestSent.connection_handle", Integer.toUnsignedLong(connectionHandle))
+                .jsonString();
+
+        int credentialHandle = CredentialApi.credentialDeserialize(serializedCredential).get();
+
+        int credentialState = CredentialApi.credentialUpdateState(credentialHandle).get();
+        if (credentialState == VcxState.Accepted.getValue()) {
+            logger.info("#16 Accepted credential from faber");
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+
+        //Serialize the object
+        serializedCredential = CredentialApi.credentialSerialize(credentialHandle).get();
+        logger.config("updateRecordWallet (credential, " + threadId + ", " + prettyJson(serializedCredential) + ")");
+        WalletApi.updateRecordWallet("credential", threadId, serializedCredential).get();
+
+        CredentialApi.credentialRelease(credentialHandle);
+    }
+
+    void sendProof(int connectionHandle, String pwDid, String msgUid) throws Exception {
+        String requests = DisclosedProofApi.proofGetRequests(connectionHandle).get();
+        String proofRequest = JsonPath.parse((LinkedHashMap)JsonPath.read(requests, "$.[0]")).jsonString();
+        logger.info("proof request:\n" + prettyJson(proofRequest));
+
+        // Update agency message status manually (xxxUpdateState automatically update message status, but not here)
+        UtilsApi.vcxUpdateMessages("MS-106",
+                "[{\"pairwiseDID\":\"" + pwDid + "\",\"uids\":[\"" + msgUid + "\"]}]");
+
+        logger.info("#23 Create a Disclosed proof object from proof request");
+        int proofHandle = DisclosedProofApi.proofCreateWithRequest("proof", proofRequest).get();
+
+        logger.info("#24 Query for credentials in the wallet that satisfy the proof request");
+        String credentials = DisclosedProofApi.proofRetrieveCredentials(proofHandle).get();
+
+        LinkedHashMap<String, Object> attrs = JsonPath.read(credentials, "$.attrs");
+        for(String key : attrs.keySet()) {
+            // use first credential (just one credential in demo)
+            String attr = JsonPath.parse((LinkedHashMap)JsonPath.read(credentials, "$.attrs." + key + ".[0]")).jsonString();
+            credentials = JsonPath.parse(credentials).set("$.attrs." + key, JsonPath.parse("{\"credential\":"+ attr + "}").json()).jsonString();
+
+            // prepare tails file and add attribute
+            String revRegId = JsonPath.read(attr, "$.cred_info.rev_reg_id");
+            String tailsFileDir = tailsFileRoot + "/" + revRegId;
+            if (Files.notExists(Paths.get(tailsFileDir))) {
+                // get tails file from tails file server
+                byte[] fileContent = requestGETtoBytes(tailsServerUrl + "/" + revRegId);
+
+                // get file name by hashing
+                String tailsFileName = getHash(fileContent);
+
+                // write tails file into tailsFileDir
+                String tailsFilePath = tailsFileDir + "/" + tailsFileName;
+                Files.createDirectory(Paths.get(tailsFileDir));
+                Files.write(Paths.get(tailsFilePath), fileContent);
+            }
+            credentials = JsonPath.parse(credentials).put("$.attrs." + key, "tails_file", tailsFileDir).jsonString();
+        }
+
+        logger.info("#25 Generate the proof");
+        DisclosedProofApi.proofGenerate(proofHandle, credentials, "{}").get();
+
+        logger.info("#26 Send the proof to faber");
+        DisclosedProofApi.proofSend(proofHandle, connectionHandle).get();
+
+        //Serialize the object
+        String serializedProof = DisclosedProofApi.proofSerialize(proofHandle).get();
+        String threadId = JsonPath.read(serializedProof,"$.data.prover_sm.thread_id");
+        logger.config("addRecordWallet (proof, " + threadId + ", " + prettyJson(serializedProof) + ")");
+        WalletApi.addRecordWallet("proof", threadId, serializedProof, "").get();
+
+        DisclosedProofApi.proofRelease(proofHandle);
+    }
+
+    void receiveProofAck(int connectionHandle, String payloadMessage) throws Exception {
+        String threadId = JsonPath.read(payloadMessage,"$.~thread.thid");
+        String proofRecord = WalletApi.getRecordWallet("proof", threadId, "").get();
+        String proof = JsonPath.read(proofRecord,"$.value");
+
+        // TODO: Must replace connection_handle in credential - Need to consider better way
+        proof = JsonPath.parse(proof)
+                .set("$.data.prover_sm.state.PresentationSent.connection_handle", Integer.toUnsignedLong(connectionHandle))
+                .jsonString();
+
+        int proofHandle = DisclosedProofApi.proofDeserialize(proof).get();
+        int proofState = DisclosedProofApi.proofUpdateState(proofHandle).get();
+
+        if (proofState == VcxState.Accepted.getValue()) {
+            logger.info("Faber received the proof");
+        }
+        else if (proofState == VcxState.None.getValue()) {
+            logger.info("Faber denied the proof (possibly the credential has been revoked)");
+            System.exit(0);
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+
+        String serializedProof = DisclosedProofApi.proofSerialize(proofHandle).get();
+        logger.config("updateRecordWallet (proof, " + threadId + ", " + prettyJson(serializedProof) + ")");
+        WalletApi.updateRecordWallet("proof", threadId, serializedProof).get();
+
+        DisclosedProofApi.proofRelease(proofHandle);
+
+        // Supposed here is the end
+        logger.info("Alice demo is completed (Exit manually)");
     }
 }
--- src/main/java/webhook/faber/GlobalService.java
+++ src/main/java/webhook/faber/GlobalService.java
@@ -3,6 +3,9 @@ package webhook.faber;
 import com.evernym.sdk.vcx.connection.ConnectionApi;
 import com.evernym.sdk.vcx.credentialDef.CredentialDefApi;
 import com.evernym.sdk.vcx.credentialDef.CredentialDefPrepareForEndorserResult;
+import com.evernym.sdk.vcx.issuer.IssuerApi;
+import com.evernym.sdk.vcx.proof.GetProofResult;
+import com.evernym.sdk.vcx.proof.ProofApi;
 import com.evernym.sdk.vcx.schema.SchemaApi;
 import com.evernym.sdk.vcx.utils.UtilsApi;
 import com.evernym.sdk.vcx.vcx.VcxApi;
@@ -13,6 +16,9 @@ import okhttp3.MultipartBody;
 import okhttp3.RequestBody;
 import org.springframework.stereotype.Service;
 import utils.Common;
+import utils.NotificationsRequestDto;
+import utils.ProofState;
+import utils.VcxState;
 
 import javax.annotation.PostConstruct;
 import java.io.File;
@@ -189,5 +195,257 @@ public class GlobalService {
         logger.config("addRecordWallet - (connection, " + pwDid + ", " + prettyJson(serializedConnection) + ")");
         WalletApi.addRecordWallet("connection", pwDid, serializedConnection, "").get();
         ConnectionApi.connectionRelease(connectionHandle);
+    }
+
+    public String getInvitation() throws Exception {
+        logger.info("getInvitation >>>");
+        String invitationRecord = WalletApi.getRecordWallet("invitation", "defaultInvitation", "").get();
+        String invitation = JsonPath.read(invitationRecord,"$.value");
+        logger.info("getInvitation <<< invitation:" + invitation);
+        return invitation;
+    }
+
+    public void handleMessage(NotificationsRequestDto body) throws Exception {
+        logger.info("handleMessage >>> body: " + body.toString());
+
+        // Get the message from mediator using notification information
+        String messages = UtilsApi.vcxGetMessages(body.getMsgStatusCode(), body.getMsgUid(), body.getPwDid()).get();
+        logger.info( "Messages: " +  messages);
+        String message = JsonPath.parse((LinkedHashMap)JsonPath.read(messages,"$.[0].msgs[0]")).jsonString();
+        String decryptedPayload = JsonPath.read(message, "$.decryptedPayload");
+        String payloadMessage = JsonPath.read(decryptedPayload,"$.@msg");
+        String type = JsonPath.read(decryptedPayload,"$.@type.name");
+
+        // pwDid is used as a connectionId
+        String pwDid = JsonPath.read(messages,"$.[0].pairwiseDID");
+        String connectionRecord = WalletApi.getRecordWallet("connection", pwDid, "").get();
+        String serializedConnection = JsonPath.read(connectionRecord,"$.value");
+        int connectionHandle = ConnectionApi.connectionDeserialize(serializedConnection).get();
+        switch(type) {
+            case "aries":
+                String innerType = JsonPath.read(payloadMessage,"$.@type");
+                // STEP.3 - update connection from F to F2A
+                if (innerType.equals("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/connections/1.0/request")) {
+                    logger.info("- Case(aries, connections/1.0/request) -> acceptConnectionRequest");
+                    acceptConnectionRequest(connectionHandle);
+                }
+                // STEP.5 - receive connection created ACK
+                else if(innerType.equals("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/notification/1.0/ack")){
+                    logger.info("- Case(aries, notification/1.0/ack) -> receiveConnectionAck & sendCredentialOffer");
+                    receiveConnectionAck(connectionHandle, pwDid);
+
+                    // STEP.6 - send credential offer
+                    sendCredentialOffer(connectionHandle);
+                }
+                // STEP.8 - send credential
+                else if(innerType.equals("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/request-credential")) {
+                    logger.info("- Case(aries ,issue-credential/1.0/request-credential) -> sendCredential");
+                    sendCredential(connectionHandle, payloadMessage);
+                }
+                // STEP.10 - request proof
+                else if(innerType.equals("did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/issue-credential/1.0/ack")) {
+                    logger.info("- Case(aries ,issue-credential/1.0/ac) -> sendProofRequest");
+                    sendProofRequest(connectionHandle);
+                }
+                else {
+                    logger.severe("Unknown innerType message in aries type");
+                }
+                break;
+            case "presentation":
+                // STEP.12 - receive & verify proof
+                logger.info("- Case(presentation)");
+                verifyProof(connectionHandle, payloadMessage);
+                break;
+            default:
+                logger.severe("Unknown type message");
+
+        }
+        ConnectionApi.connectionRelease(connectionHandle);
+    }
+
+    void acceptConnectionRequest(int connectionHandle) throws Exception {
+        int connectionState = ConnectionApi.vcxConnectionUpdateState(connectionHandle).get();
+        if (connectionState == VcxState.RequestReceived.getValue()) {
+            // new relationship
+            String newPwDid = ConnectionApi.connectionGetPwDid(connectionHandle).get();
+
+            String serializedConnection = ConnectionApi.connectionSerialize(connectionHandle).get();
+            logger.config("addRecordWallet (connection, " + newPwDid + ", " + prettyJson(serializedConnection) + ")");
+            WalletApi.addRecordWallet("connection", newPwDid, serializedConnection, "").get();
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+    }
+
+    void receiveConnectionAck(int connectionHandle, String pwDid) throws Exception {
+        int connectionState = ConnectionApi.vcxConnectionUpdateState(connectionHandle).get();
+        if (connectionState == VcxState.Accepted.getValue()) {
+            String serializedConnection = ConnectionApi.connectionSerialize(connectionHandle).get();
+            logger.config("updateRecordWallet (connection, " + pwDid + ", " + prettyJson(serializedConnection) + ")");
+            WalletApi.updateRecordWallet("connection", pwDid, serializedConnection).get();
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+    }
+
+    void sendCredentialOffer(int connectionHandle) throws Exception {
+        String schemaAttrs = JsonPath.parse("{" +
+                "  name: 'alice'," +
+                "  last_name: 'clark'," +
+                "  date: '05-2018'," +
+                "  degree: 'maths'," +
+                "  age: '25'" +
+                "}").jsonString();
+
+        logger.info("#12 Create an IssuerCredential object using the schema and credential definition\n"
+                + prettyJson(schemaAttrs));
+
+        String credDefRecord = WalletApi.getRecordWallet("credentialDef", "defaultCredentialDef", "").get();
+        String credDef = JsonPath.read(credDefRecord,"$.value");
+        int credDefHandle = CredentialDefApi.credentialDefDeserialize(credDef).get();
+
+        int credentialHandle = IssuerApi.issuerCreateCredential("alice_degree",
+                credDefHandle,
+                null,
+                schemaAttrs,
+                "cred",
+                0).get();
+        logger.info("#13 Issue credential offer to alice");
+        IssuerApi.issuerSendCredentialOffer(credentialHandle, connectionHandle).get();
+
+        String serializedCredential = IssuerApi.issuerCredentialSerialize(credentialHandle).get();
+        String threadId = JsonPath.read(serializedCredential,"$.data.issuer_sm.state.OfferSent.thread_id");
+        logger.config("addRecordWallet (credential, " + threadId + ", " + prettyJson(serializedCredential) + ")");
+        WalletApi.addRecordWallet("credential", threadId, serializedCredential, "").get();
+
+        IssuerApi.issuerCredentialRelease(credentialHandle);
+        CredentialDefApi.credentialDefRelease(credDefHandle);
+    }
+
+    void sendCredential(int connectionHandle, String payloadMessage) throws Exception {
+        String threadId = JsonPath.read(payloadMessage,"$.~thread.thid");
+        String credentialRecord = WalletApi.getRecordWallet("credential", threadId, "").get();
+        String serializedCredential = JsonPath.read(credentialRecord,"$.value");
+
+        // TODO: Must replace connection_handle in credential - Need to consider better way
+        serializedCredential = JsonPath.parse(serializedCredential)
+                .set("$.data.issuer_sm.state.OfferSent.connection_handle", Integer.toUnsignedLong(connectionHandle))
+                .jsonString();
+
+        int credentialHandle = IssuerApi.issuerCredentialDeserialize(serializedCredential).get();
+        int credentialState = IssuerApi.issuerCredentialUpdateState(credentialHandle).get();
+
+        if (credentialState == VcxState.RequestReceived.getValue()) {
+            logger.info("#17 Issue credential to alice");
+            IssuerApi.issuerSendCredential(credentialHandle, connectionHandle).get();
+
+            serializedCredential = IssuerApi.issuerCredentialSerialize(credentialHandle).get();
+            logger.config("updateRecordWallet (credential, " + threadId + ", " + prettyJson(serializedCredential) + ")");
+            WalletApi.updateRecordWallet("credential", threadId, serializedCredential).get();
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+        IssuerApi.issuerCredentialRelease(credentialHandle);
+    }
+
+    void sendProofRequest(int connectionHandle) throws Exception {
+        String vcxConfigRecord = WalletApi.getRecordWallet("vcxConfig", "defaultVcxConfig", "").get();
+        String vcxConfig = JsonPath.read(vcxConfigRecord,"$.value");
+
+        String proofAttributes = JsonPath.parse("[" +
+                "  {" +
+                "    names: ['name', 'last_name']," +
+                "    restrictions: [{ issuer_did: " + JsonPath.read(vcxConfig, "$.institution_did") + " }]" +
+                "  }," +
+                "  {" +
+                "    name: 'date'," +
+                "    restrictions: { issuer_did: " + JsonPath.read(vcxConfig, "$.institution_did") + " }" +
+                "  }," +
+                "  {" +
+                "    name: 'degree'," +
+                "    restrictions: { 'attr::degree::value': 'maths' }" +
+                "  }" +
+                "]").jsonString();
+
+        String proofPredicates = JsonPath.parse("[" +
+                "  {" +
+                "    name: 'age'," +
+                "    p_type: '>='," +
+                "    p_value: 20," +
+                "    restrictions: [{ issuer_did: " + JsonPath.read(vcxConfig, "$.institution_did") + " }]" +
+                "  }" +
+                "]").jsonString();
+
+        long curUnixTime = System.currentTimeMillis() / 1000L;
+        String revocationInterval = "{\"to\": " + curUnixTime + "}";
+
+        logger.info("#19 Create a Proof object\n" +
+                "proofAttributes: " + prettyJson(proofAttributes) + "\n" +
+                "proofPredicates: " + prettyJson(proofPredicates) + "\n" +
+                "revocationInterval: " + prettyJson(revocationInterval));
+        int proofHandle = ProofApi.proofCreate("proof_uuid",
+                proofAttributes,
+                proofPredicates,
+                revocationInterval,
+                "proof_from_alice").get();
+
+        logger.info("#20 Request proof of degree from alice");
+        ProofApi.proofSendRequest(proofHandle, connectionHandle).get();
+
+        String serializedProof = ProofApi.proofSerialize(proofHandle).get();
+        String threadId = JsonPath.read(serializedProof,"$.data.verifier_sm.state.PresentationRequestSent.presentation_request.@id");
+        logger.config("addRecordWallet (proof, " + threadId + ", " + prettyJson(serializedProof) + ")");
+        WalletApi.addRecordWallet("proof", threadId, serializedProof, "").get();
+
+        ProofApi.proofRelease(proofHandle);
+    }
+
+    void verifyProof(int connectionHandle, String payloadMessage) throws Exception {
+        String threadId = JsonPath.read(payloadMessage, "$.~thread.thid");
+        String proofRecord = WalletApi.getRecordWallet("proof", threadId, "").get();
+        String serializedProof = JsonPath.read(proofRecord, "$.value");
+
+        // TODO: Must replace connection_handle in proof - Need to consider better way
+        serializedProof = JsonPath.parse(serializedProof)
+                .set("$.data.verifier_sm.state.PresentationRequestSent.connection_handle", Integer.toUnsignedLong(connectionHandle))
+                .jsonString();
+
+        int proofHandle = ProofApi.proofDeserialize(serializedProof).get();
+        int proofState = ProofApi.proofUpdateState(proofHandle).get();
+
+        if (proofState == VcxState.Accepted.getValue()) {
+            logger.info("#27 Process the proof provided by alice");
+            GetProofResult proofResult = ProofApi.getProof(proofHandle, connectionHandle).get();
+
+            logger.info("#28 Check if proof is valid");
+            if (proofResult.getProof_state() == ProofState.Validated.getValue()) {
+                String encodedProof = JsonPath.read(proofResult.getResponse_data(), "$.presentations~attach.[0].data.base64");
+                String decodedProof = decodeBase64(encodedProof);
+                String requestedProof = JsonPath.parse((LinkedHashMap)JsonPath.read(decodedProof, "$.requested_proof")).jsonString();
+                logger.info("Requested proof:" + prettyJson(requestedProof));
+                logger.info("Proof is verified");
+            }
+            else if (proofResult.getProof_state() == ProofState.Invalid.getValue()) {
+                logger.warning("Proof verification failed. credential has been revoked");
+            }
+            else {
+                logger.warning("Unexpected proof state" + proofResult.getProof_state());
+            }
+        }
+        else if (proofState == VcxState.None.getValue()) {
+            logger.info("Incorrect proof is received");
+        }
+        else {
+            logger.severe("Unexpected state type");
+        }
+
+        serializedProof = ProofApi.proofSerialize(proofHandle).get();
+        logger.config("updateRecordWallet (proof, " + threadId + ", " + prettyJson(serializedProof) + ")");
+        WalletApi.updateRecordWallet("proof", threadId, serializedProof).get();
+
+        ProofApi.proofRelease(proofHandle);
     }
 }
